7e45ee5770767703c0fa5c979a0d11ec406b518d
MaxVerevkin <34583604+MaxVerevkin@users.noreply.github.com>
2022-01-02 16:25:45 +0000

    Deploying to gh-pages from @ MaxVerevkin/swaystatus@611277aad5ae57d07e7c9160ca3c3390857eb3b7 🚀

--- swaystatus/blocks/github/sidebar-items.js
+++ swaystatus/blocks/github/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["default_hide",""],["default_interval",""],["get_total",""],["run",""]],"struct":[["GithubConfig",""]]});
+initSidebarItems({"fn":[["get_on_page",""],["get_stats",""],["run",""]],"struct":[["GithubConfig",""],["Notification",""]]});
